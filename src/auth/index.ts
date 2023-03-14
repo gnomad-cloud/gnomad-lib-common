b0b336f6a38c2a8a5c61533b17e4697a8a8745ae
Gypsy Gnomad <gyspy@gnomad.life>
2023-03-15 01:31:07 +1100

    I_JWT

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -2,9 +2,11 @@ export interface I_Authenticator {
     connect(): void;
 }
 
-export interface I_Entitlement {
+export interface I_Guard {
     allowed(jwt: I_JWT, permission: string): Promise<boolean>;
+}
 
+export interface I_Entitlement extends I_Guard {
     grant(jwt: I_JWT, permission: string): Promise<any>;
     revoke(jwt: I_JWT, permission: string): Promise<any>;
     forget(jwt: I_JWT): Promise<any>;
