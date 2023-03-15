a921ce2827924e9b2ac506128aae198accb34b96
Gypsy Gnomad <gyspy@gnomad.life>
2023-03-15 21:34:40 +1100

    fix

--- src/store/abstract.ts
+++ src/store/abstract.ts
@@ -4,6 +4,7 @@ import path from "path"
 import { createHash } from 'node:crypto'
 
 import { I_Store, I_StoredFile } from ".";
+import Fault from "../utils/Fault";
 
 export abstract class AbstractFileStore<T> implements I_Store<T> {
     FILE_TYPE = "json";
@@ -18,9 +19,16 @@ export abstract class AbstractFileStore<T> implements I_Store<T> {
         return createHash('sha256').update(content).digest('hex')
     }
 
+    validateIRI(iri: string) {
+        if (/^(http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/g.test(iri))
+            return iri;
+        throw new Fault("gnomad.store.iri.invalid", { iri })
+    }
+
 
     resolve(file: string): string {
-        const ix = file.indexOf("://");
+        file = this.validateIRI(file);
+        let ix = file.indexOf("://");
         if (ix>=0) file = file.substring(ix+3);
         // console. log("resolve: %s -> %s", ix, file)
         return path.join(this.basePath, file + "." + this.FILE_TYPE);
