d058e9ab1639bf2bf333a587072a991222604ee1
Gypsy Gnomad <gyspy@gnomad.life>
2023-03-15 12:35:29 +1100

    refactor

--- src/events/cloud.ts
+++ src/events/cloud.ts
@@ -11,9 +11,7 @@ export class EventBroker implements I_Broker {
         if (process.env.NODE_ENV=='development') CE_BROKER = CE_BROKER || "http://localhost:3000/ce/debug"; 
 
         if (!CE_BROKER) throw new Error("missing CE_BROKER");
-
         console.log("ce.broker: %s", CE_BROKER)
-
         this.emit = emitterFor(httpTransport(CE_BROKER as string));
     }
 
